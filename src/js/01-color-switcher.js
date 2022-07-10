033e3552e9d26073f572b2fe646c5acb4f2171cf
Stepan Kryukov <stephenkryukov@gmail.com>
2022-07-10 23:22:06 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body');
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+let timer = null;
 
+start.addEventListener('click', startChangeBgColor);
+stop.addEventListener('click', stopChangeBgColor);
+
+function startChangeBgColor() {
+  timer = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  start.disabled = true;
+  stop.disabled = false;
+}
+
+function stopChangeBgColor() {
+  clearInterval(timer);
+  start.disabled = false;
+  stop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
